8766ded6a018ffaa266353651260814768c0a1d5
TheGamingSnail <87595446+TheGamingSnail@users.noreply.github.com>
2021-08-07 10:46:03 +0300

    added perm and changed up code a bit

--- src/GamingSnail7410/GrantPotatoes/Main.php
+++ src/GamingSnail7410/GrantPotatoes/Main.php
@@ -14,23 +14,23 @@ use pocketmine\command\Command;
 
 use pocketmine\command\CommandSender;
 
-class Main extends PluginBase implements Listener{
-   public function onEnabled(){
-    $this->getServer()->getPluginManager()->registerEvents($this,$this);
-    $this->getLogger()->info("Plugin is Enabled");
-
-   }
-   public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
-    switch($cmd->getName()){
-        case "potato":
-if(!$sender instanceof Player){
-          $sender->sendMessage("This Command Only Works in-game!");
-     }else{
-          $sender->getInventory()->addItem(Item::get(393,0,$args[0])); 
-          $sender->sendMessage("You have just recieved". count($args[0]) . " baked potatoes!");
-     }
-}
-return true;
-    }
-
+public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
+   switch($cmd->getName()){
+     case "test":
+       if($sender->hasPermission("potato.command")){
+         if(!$sender instanceof Player){
+           $sender->sendMessage("This Command Only Works In-Game!");
+         }else{
+           if(!isset($args[0]) or (is_int($args[0]) and $args[0] > 0)){ 
+            $args[0] = 1; 
+           }
+           $sender->getInventory()->addItem(Item::get(393,0,$args[0]));
+           $sender->sendMessage("You have just recieved" .count($args[0]). " baked potatoes!");
+         }
+       }else{
+         $sender->sendMessage("You don't have permission to use these potatoes");
+       }
+     break;
+  }
+  return true;
 }
